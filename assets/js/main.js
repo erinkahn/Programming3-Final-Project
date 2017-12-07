fe4a0b98e5683ccdbdda43f6a85d005512e05802
erinkahn <erin.kahn@atlanta.creativecircus.edu>
2017-12-07 11:14:55 -0500

    submit button and alerts styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,92 +185,89 @@ var straightUpForm = function() {
 		console.log('validate page 1');
 		// console.log('validate page 1');
 
-		// name field-----------------
-		var nameValid = false;
+		// order is backwards because of alerts calling bottom to top instead
+		// of top to bototm
 		
-		if (name.value !== '' && typeof name.value === 'string') {
-			console.log('my name is', name.value);
-			addClass(name, 'valid');
-			removeClass(name, 'invalid');
-			nameValid = true;
-			
 
-		} else if (name.value === '' || typeof name.value !== 'string') {
-			alert("Error: Enter your name, damnit!");
-			console.log("you didnt enter your name");
-			name.focus();
-			addClass(name, 'invalid');
-			removeClass(name, 'valid');
+
+		// lookingfor field-----------------
+
+
+		var lookingForValid = false;
+
+		for (var i = 0; i < lookingForSelected.length; i++) {
+			var lookingForSelection = lookingForSelected[i];
+
+			if (lookingForSelection.checked) {
+				lookingForValid = true;
+			}
 		}
-
-
-
-		// gender field-----------------
-
-		// if the checkbox is NOT checked, its male
-		// if the checkbox is checked, its female
-
-
-		if (!genderChecked.checked || genderChecked == null) {
-			addClass(gender, 'female');
-			removeClass(gender, 'male');
-			// console.log("I'm a male");
-			
-		} else {
-			addClass(gender, 'male');
-			removeClass(gender, 'female');
-			// console.log("I am a female!");
-		}
-
-
-
-		// user age field-----------------
-
-		var ageValid = false;
-
-		if (ageSlider.value >= 21) {
-			// console.log('you are of age');
-			addClass(age, 'valid');
-			removeClass(age, 'invalid');
-			ageValid = true;
+		
+		if (lookingForValid) {
+			addClass(lookingFor, 'valid');
+			removeClass(lookingFor, 'invalid');
+			// console.log("looking for is selected");
 
 		} else {
-			alert("Error: You're a baby. GTFO.");
-			// console.log("Youre a baby. GTFO");                                  
-			age.focus();
-			addClass(age, 'invalid');
-			removeClass(age, 'valid');
+			// alert("ERROR: You didn't choose what you're looking for");
+			swal({
+			  title: 'Error!',
+			  text: 'You did not choose what you are looking for.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			lookingFor.focus();
+			addClass(lookingFor, 'invalid');
+			removeClass(lookingFor, 'valid');
 		}
+
+
+		
+
+
+		// bodytype field-----------------
+
+
+		var bodValid = false;
+
+		for (var i = bodySelected.length - 1; i >= 0; i--) {
+			bodySelected[i];
+
+			if(!bodySelected[i].selected){
+                addClass(bodyType, 'invalid');
+				removeClass(bodyType, 'valid');
+			} else {
+				bodValid = true;
+				addClass(bodyType, 'valid');
+				removeClass(bodyType, 'invalid');
+			}
+		}
+
+		if (!bodValid) {
+			bodyType.focus();
+			// alert("Error: You didn't select a body type");
+			swal({
+			  title: 'Error!',
+			  text: 'You left out your body type.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			// console.log("select a body type");
+		} else {
+			// console.log("yay. a body type was selected!");
+		}
+
+
+
+		
 
 	
 
 
 
-		// haircolor field-----------------
+		
 
-		var hairValid = false;
-
-		for (var i = hairSelected.length - 1; i >= 0; i--) {
-				hairSelected[i];
-
-			if (!hairSelected[i].checked) {
-				addClass(hair, 'invalid');
-				removeClass(hair, 'valid');
-
-			} else {
-				hairValid = true;
-				addClass(hair, 'valid');
-				removeClass(hair, 'invalid');
-			}
-		}
-
-		if (!hairValid) {
-			hair.focus();
-			alert("ERROR: Your hair color is NOT selected");
-			// console.log("Your hair color is NOT selected");
-		} else {
-			// console.log("hair color was selected");
-		}
+		
 
 
 		// users height field-----------------
@@ -293,7 +290,13 @@ var straightUpForm = function() {
 		}
 
 		if (!heightValid) {
-			alert("ERROR: You didn't click your height");
+			// alert("ERROR: You didn't click your height");
+			swal({
+			  title: 'Error!',
+			  text: 'You did not click your height.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
 			// console.log("Your height is NOT selected");
 		} else {
 			// console.log("you chose your height");
@@ -301,59 +304,124 @@ var straightUpForm = function() {
 
 
 
-		// bodytype field-----------------
+
+
+
+
+		// haircolor field-----------------
+
+
+		var hairValid = false;
+
+		for (var i = hairSelected.length - 1; i >= 0; i--) {
+				hairSelected[i];
+
+			if (!hairSelected[i].checked) {
+				addClass(hair, 'invalid');
+				removeClass(hair, 'valid');
+
+			} else {
+				hairValid = true;
+				addClass(hair, 'valid');
+				removeClass(hair, 'invalid');
+			}
+		}
+
+		if (!hairValid) {
+			hair.focus();
+			// alert("ERROR: Your hair color is NOT selected");
+			swal({
+			  title: 'Error!',
+			  text: 'Your hair color is NOT selected.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			// console.log("Your hair color is NOT selected");
+		} else {
+			// console.log("hair color was selected");
+		}
+
+
+
+
+
+
+		// user age field-----------------
+
+		var ageValid = false;
+
+		if (ageSlider.value >= 21) {
+			// console.log('you are of age');
+			addClass(age, 'valid');
+			removeClass(age, 'invalid');
+			ageValid = true;
+
+		} else {
+			// alert("Error: You're a baby. GTFO.");
+			swal({
+			  title: 'Error!',
+			  text: 'You are a child. Please GTFO.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Fine.'
+			})
+			// console.log("Youre a baby. GTFO");                                  
+			age.focus();
+			addClass(age, 'invalid');
+			removeClass(age, 'valid');
+		}
+
+
+
+
+
+		// gender field-----------------
+
+		// if the checkbox is NOT checked, its male
+		// if the checkbox is checked, its female
+
+
+		if (!genderChecked.checked || genderChecked == null) {
+			addClass(gender, 'female');
+			removeClass(gender, 'male');
+			// console.log("I'm a male");
+			
+		} else {
+			addClass(gender, 'male');
+			removeClass(gender, 'female');
+			// console.log("I am a female!");
+		}
+
+
+
+
+
+		// name field-----------------
+
+
+		var nameValid = false;
+		
+		if (name.value !== '' && typeof name.value === 'string') {
+			console.log('my name is', name.value);
+			addClass(name, 'valid');
+			removeClass(name, 'invalid');
+			nameValid = true;
 			
 
-		var bodValid = false;
-
-		for (var i = bodySelected.length - 1; i >= 0; i--) {
-			bodySelected[i];
-
-			if(!bodySelected[i].selected){
-                addClass(bodyType, 'invalid');
-				removeClass(bodyType, 'valid');
-			} else {
-				bodValid = true;
-				addClass(bodyType, 'valid');
-				removeClass(bodyType, 'invalid');
-			}
-		}
-
-		if (!bodValid) {
-			bodyType.focus();
-			alert("Error: You didn't select a body type");
-			// console.log("select a body type");
-		} else {
-			// console.log("yay. a body type was selected!");
+		} else if (name.value === '' || typeof name.value !== 'string') {
+			// alert("Error: Enter your name, damnit!");
+			swal({
+			  title: 'Error!',
+			  text: 'You did NOT enter your name.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			console.log("you didnt enter your name");
+			name.focus();
+			addClass(name, 'invalid');
+			removeClass(name, 'valid');
 		}
 
 
-
-
-		// lookingfor field-----------------
-
-
-		var lookingForValid = false;
-
-		for (var i = 0; i < lookingForSelected.length; i++) {
-			var lookingForSelection = lookingForSelected[i];
-
-			if (lookingForSelection.checked) {
-				lookingForValid = true;
-			}
-		}
-		
-		if (lookingForValid) {
-			addClass(lookingFor, 'valid');
-			removeClass(lookingFor, 'invalid');
-			// console.log("looking for is selected");
-
-		} else {
-			alert("ERROR: You didn't choose what you're looking for");
-			lookingFor.focus();
-			addClass(lookingFor, 'invalid');
-			removeClass(lookingFor, 'valid');
-		}
 
 
 		// if validation is correct/no errors and all fields are completed, go to next fieldset
@@ -377,6 +445,136 @@ var straightUpForm = function() {
 
 
 	function validatePage2(form) {
+
+		
+		// order of fields is backwards bc of alerts calling bottom to top
+
+		// tattoos field-----------------
+
+		// if the checkbox is checked, its yes 
+		// if the checkbox is unchecked/null, its no
+		if (!tattooChecked.checked || tattooChecked == null) {
+			addClass(tattoo, 'no');
+			removeClass(tattoo, 'yes');
+			// console.log("no. i hate tattoos");
+		} else {
+			addClass(tattoo, 'yes');
+			removeClass(tattoo, 'no');
+			// console.log("hell yas. i love tattoos");
+		}
+
+
+
+		// matchsheight field-----------------
+
+
+		var matchHeightValid = false;
+
+		for (var i = matchHeightClicked.length - 1; i >= 0; i--) {
+				matchHeightClicked[i];
+
+			if (!matchHeightClicked[i].checked) {
+				addClass(matchHeight, 'invalid');
+				removeClass(matchHeight, 'valid');
+
+			} else {
+				matchHeightValid = true;
+				addClass(matchHeight, 'valid');
+				removeClass(matchHeight, 'invalid');
+			}
+		}
+
+		if (!matchHeightValid) {
+			// alert("ERROR: What height do you want them to be?");
+			swal({
+			  title: 'Error!',
+			  text: 'You forgot to tell us what height you want your match to be.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			// console.log("Your matchs height is NOT selected");
+		} else {
+			// console.log("you chose your matchs height");
+		}
+
+
+
+
+		// perfectdate field-----------------
+
+		var dateValid = false;
+
+		for (var i = 0; i < dateSelected.length; i++) {
+			dateSelected[i];
+
+			if (!dateSelected[i].selected) {
+				addClass(date, 'invalid');
+				removeClass(date, 'valid');
+
+			} else {
+				dateValid = true;
+				addClass(date, 'valid');
+				removeClass(date, 'invalid');
+			}
+		}
+
+		if (!dateValid) {
+			date.focus();
+			// alert("Error: omg whats your perfect date");
+			swal({
+			  title: 'Error!',
+			  text: 'You left out your idea of a perfect date.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			// console.log("you didnt choose your perfect date");
+		} else {
+			// console.log("you chose a perfect date");
+		}
+
+
+
+
+
+		// matchsage field-----------------
+
+		// if you choose 1 age range box, you chose correctly
+		// if its null or unchecked, Error: field is empty
+
+		var matchAgeValid = false;
+
+		for (var i = matchsAgeRangeClicked.length - 1; i >= 0; i--) {
+				matchsAgeRangeClicked[i];
+
+			if (!matchsAgeRangeClicked[i].checked) {
+				addClass(matchsAge, 'invalid');
+				removeClass(matchsAge, 'valid');
+
+			} else {
+				matchAgeValid = true;
+				addClass(matchsAge, 'valid');
+				removeClass(matchsAge, 'invalid');
+			}
+		}
+
+		if (!matchAgeValid) {
+			matchsAge.focus();
+			// alert("ERROR: What age range are you looking for?");
+			swal({
+			  title: 'Error!',
+			  text: 'You left out what age range you are looking for?',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
+			// console.log("You didn't choose an age range for your match");
+		} else {
+			// console.log("you chose an age range");
+		}
+
+
+
+
+
 
 		// typeperson field-----------------
 
@@ -409,118 +607,18 @@ var straightUpForm = function() {
 			
 		} else {
 			typePerson.focus();
-			alert("ERROR: Select one type but NO MORE than 8.");
+			// alert("ERROR: Select one type but NO MORE than 8.");
+			swal({
+			  title: 'Error!',
+			  text: 'Select one type but NO MORE than 8.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix that.'
+			})
 			// console.log("Your type is either NOT selected or you chose too many");
 		}
 
 
 
-
-		// matchsage field-----------------
-
-		// if you choose 1 age range box, you chose correctly
-		// if its null or unchecked, Error: field is empty
-
-		var matchAgeValid = false;
-
-		for (var i = matchsAgeRangeClicked.length - 1; i >= 0; i--) {
-				matchsAgeRangeClicked[i];
-
-			if (!matchsAgeRangeClicked[i].checked) {
-				addClass(matchsAge, 'invalid');
-				removeClass(matchsAge, 'valid');
-
-			} else {
-				matchAgeValid = true;
-				addClass(matchsAge, 'valid');
-				removeClass(matchsAge, 'invalid');
-			}
-		}
-
-		if (!matchAgeValid) {
-			matchsAge.focus();
-			alert("ERROR: What age range are you looking for?");
-			// console.log("You didn't choose an age range for your match");
-		} else {
-			// console.log("you chose an age range");
-		}
-
-
-
-
-		// perfectdate field-----------------
-
-		var dateValid = false;
-
-		for (var i = 0; i < dateSelected.length; i++) {
-			dateSelected[i];
-
-			if (!dateSelected[i].selected) {
-				addClass(date, 'invalid');
-				removeClass(date, 'valid');
-
-			} else {
-				dateValid = true;
-				addClass(date, 'valid');
-				removeClass(date, 'invalid');
-			}
-		}
-
-		if (!dateValid) {
-			date.focus();
-			alert("Error: omg whats your perfect date");
-			// console.log("you didnt choose your perfect date");
-		} else {
-			// console.log("you chose a perfect date");
-		}
-
-
-
-
-
-		// matchsheight field-----------------
-
-
-		var matchHeightValid = false;
-
-		for (var i = matchHeightClicked.length - 1; i >= 0; i--) {
-				matchHeightClicked[i];
-
-			if (!matchHeightClicked[i].checked) {
-				addClass(matchHeight, 'invalid');
-				removeClass(matchHeight, 'valid');
-
-			} else {
-				matchHeightValid = true;
-				addClass(matchHeight, 'valid');
-				removeClass(matchHeight, 'invalid');
-			}
-		}
-
-		if (!matchHeightValid) {
-			alert("ERROR: What height do you want them to be?");
-			// console.log("Your matchs height is NOT selected");
-		} else {
-			// console.log("you chose your matchs height");
-		}
-
-
-
-
-
-		// tattoos field-----------------
-
-		// if the checkbox is checked, its yes 
-		// if the checkbox is unchecked/null, its no
-		if (!tattooChecked.checked || tattooChecked == null) {
-			addClass(tattoo, 'no');
-			removeClass(tattoo, 'yes');
-			// console.log("no. i hate tattoos");
-		} else {
-			addClass(tattoo, 'yes');
-			removeClass(tattoo, 'no');
-			// console.log("hell yas. i love tattoos");
-		}
 
 
 		// if validation is correct/no errors and all fields are completed, go to next fieldset
@@ -547,6 +645,62 @@ var straightUpForm = function() {
 		// console.log('validate page 3');
 
 
+		// order is backwards because of alerts calling bottom to top
+
+		
+		// confirm password -----------
+
+		var confirmValid = false;
+
+		if (confirmPw.value != "" && confirmPw.value == password.value) {
+			addClass(confirmPw, 'valid');
+			removeClass(confirmPw, 'invalid');
+			confirmValid = true;
+			// console.log('yay confirmation pw matches!!!');
+		} else {
+			// alert("Error: your confirmation password isnt correct!");
+			swal({
+			  title: 'Error!',
+			  text: 'Your confirmation password isnt correct!',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix it.'
+			})
+	        // console.log('confirmation password is wrong');
+	        confirmPw.focus();
+	        addClass(confirmPw, 'invalid');
+			removeClass(confirmPw, 'valid');
+		}
+
+
+
+
+		// password -----------------
+
+		var passwordValid = false;
+		
+		if (password.value != "" && password.value.length >= 6) {
+			addClass(password, 'valid');
+			removeClass(password, 'invalid');
+			passwordValid = true;
+			// console.log('yay you followed instructions');
+	        
+	    } else {
+			// alert("Error: Password must contain at least six characters!");
+			swal({
+			  title: 'Error!',
+			  text: 'Password must contain at least six characters!',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix it.'
+			})
+	        // console.log('you didnt enter a correct password');
+	        password.focus();
+	        addClass(password, 'invalid');
+			removeClass(password, 'valid');
+		}
+
+
+
+
 		// email field-----------------
 	
 		var emailValid = false;
@@ -558,50 +712,17 @@ var straightUpForm = function() {
 			// console.log('you entered a CORRECT email');
 
 		} else {
-			alert("Error: Enter a valid email address.");
+			// alert("Error: Enter a valid email address.");
+			swal({
+			  title: 'Error!',
+			  text: 'Enter a valid email address.',
+			  type: 'error',
+			  confirmButtonText: 'Ok. Lemme fix it.'
+			})
 			// console.log('you entered the wrong email address')
 			email.focus();
 			addClass(email, 'invalid');
 			removeClass(email, 'valid');
-		}
-
-
-
-
-		// password -----------------
-
-		var passwordValid = false;
-		
-		if (password.value != "" && password.value.length > 6) {
-			addClass(password, 'valid');
-			removeClass(password, 'invalid');
-			passwordValid = true;
-			// console.log('yay you followed instructions');
-	        
-	    } else {
-			alert("Error: Password must contain at least six characters!");
-	        // console.log('you didnt enter a correct password');
-	        password.focus();
-	        addClass(password, 'invalid');
-			removeClass(password, 'valid');
-		}
-
-
-		// confirm password -----------
-
-		var confirmValid = false;
-
-		if (confirmPw.value != "" && confirmPw.value == password.value) {
-			addClass(confirmPw, 'valid');
-			removeClass(confirmPw, 'invalid');
-			confirmValid = true;
-			// console.log('yay confirmation pw matches!!!');
-		} else {
-			alert("Error: your confirmation password isnt correct!");
-	        // console.log('confirmation password is wrong');
-	        confirmPw.focus();
-	        addClass(confirmPw, 'invalid');
-			removeClass(confirmPw, 'valid');
 		}
 
 
@@ -670,14 +791,11 @@ var straightUpForm = function() {
 		e.preventDefault();
 		if ( validatePage3() ) {
 			console.log("page three validated");
-			alert(`Congrats! Your account is created. Now, let's find you a match!`);
+			swal("Congrats!", "Your account has been created!", "success");
 		}
 	});
 
 
-	// theForm.addEventListener("submit", function(e) {
-	// 	e.preventDefault();
-	// });
 
 };
 
